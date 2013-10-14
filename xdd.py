623a193dd152a7ca00681e985b816c997b91babf
Deepak Sarda <deepak@antrix.net>
2013-10-15 07:12:27 +0800

    read random aphorism from database and show on home page

--- xdd.py
+++ xdd.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, g, abort
+
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
+
+RDB_HOST =  os.environ.get('RDB_HOST') or 'localhost'
+RDB_PORT = os.environ.get('RDB_PORT') or 28015
+RDB_DB = 'xdd'
 
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    try:
+        g.rdb_conn = r.connect(host=RDB_HOST, port=RDB_PORT, db=RDB_DB)
+    except RqlDriverError:
+        abort(503, "No database connection could be established.")
+
+@app.teardown_request
+def teardown_request(exception):
+    try:
+        g.rdb_conn.close()
+    except AttributeError:
+        pass
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    aphorism = list(r.table('aphorisms').sample(1).run(g.rdb_conn))[0]
+    return render_template('index.html', aphorism=aphorism)
 
 if __name__ == '__main__':
     app.run(debug=True)
