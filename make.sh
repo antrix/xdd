88c2c5f31311484c455e4554f066a4e45aec8e30
Deepak Sarda <deepak@antrix.net>
2013-10-31 07:03:38 +0800

    add a trivial build script

---
+++ make.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+OUTPUT_DIR="./output"
+STATIC_DIR="./static"
+
+
+rm -rf ${OUTPUT_DIR}
+python compile.py
+
+cp -r ${STATIC_DIR} ${OUTPUT_DIR}
+
+find ${OUTPUT_DIR} -type f | xargs chmod 644
+find ${OUTPUT_DIR} -type d | xargs chmod 755
