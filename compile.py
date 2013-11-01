56b58b23d747b99c384baa07eec647f5e8d2a8c8
Deepak Sarda <deepak@antrix.net>
2013-11-01 13:11:34 +0800

    refactor compile.py extracting methods and such

--- compile.py
+++ compile.py
@@ -19,45 +19,61 @@ content_ext = '.json'
 
 ## End configuration options ##
 
-template_loader = FileSystemLoader(template_dir)
-env = Environment(autoescape=False, loader=template_loader)
-template = env.get_template("index.jnj")
+def get_content_list():
+    content_files = ((f, os.path.join(content_dir, f)) for f in os.listdir(content_dir) if f.endswith(content_ext))
 
-content_files = ((f, os.path.join(content_dir, f)) for f in os.listdir(content_dir) if f.endswith(content_ext))
+    content_list = []
 
-context_list = []
+    for f, fpath in content_files:
+        #print "Now file:", f
+        fname, ext = os.path.splitext(f)
 
-for f, fpath in content_files:
-    #print "Now file:", f
-    fname, ext = os.path.splitext(f)
+        context = json.loads(open(fpath).read())
 
-    context = json.loads(open(fpath).read())
+        context["date"] = iso8601.parse_date(context["date"])
+        context["name"] = fname
+        context["slug"] = "/%s/" % (fname,)
 
-    context["date"] = iso8601.parse_date(context["date"])
-    context["name"] = fname
-    context["slug"] = "/%s/" % (fname,)
+        content_list.append(context)
 
-    context_list.append(context)
+    content_list.sort(key=lambda c: c["date"])
 
-context_list.sort(key=lambda c: c["date"])
+    for idx, context in enumerate(content_list):
+        context["prev_slug"] = content_list[idx - 1]["slug"] if idx > 0 else None
+        context["next_slug"] = content_list[idx + 1]["slug"] if idx < len(content_list) - 1 else None
 
-for idx, context in enumerate(context_list):
-    context["prev_slug"] = context_list[idx - 1]["slug"] if idx > 0 else None
-    context["next_slug"] = context_list[idx + 1]["slug"] if idx < len(context_list) - 1 else None
+    return content_list
 
 
-for context in context_list:
-    #print "Now name:", context["name"], context["date"]
+def gen_html_pages(content_list, jinja_env):
 
-    o = template.render(context)
+    template = jinja_env.get_template("index.jnj")
 
-    dest_dir = os.path.join(output_dir, context["name"])
+    for context in content_list:
+        #print "Now name:", context["name"], context["date"]
 
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
+        o = template.render(context)
 
-    output_file = os.path.join(dest_dir, 'index.html')
+        dest_dir = os.path.join(output_dir, context["name"])
 
-    open(output_file, 'w').write(o.encode("utf-8"))
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
 
-shutil.copy(os.path.join(output_dir, context_list[-1]["name"], "index.html"), output_dir)
+        output_file = os.path.join(dest_dir, 'index.html')
+
+        open(output_file, 'w').write(o.encode("utf-8"))
+
+    shutil.copy(os.path.join(output_dir, content_list[-1]["name"], "index.html"), output_dir)
+
+def main():
+
+    content_list = get_content_list()
+
+    template_loader = FileSystemLoader(template_dir)
+    env = Environment(autoescape=False, loader=template_loader)
+
+    gen_html_pages(content_list, env)
+
+if __name__ == '__main__':
+    main()
+
