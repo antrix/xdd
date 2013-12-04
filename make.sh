1a9219d7dd9a5db21b742e1c6a10a27443e5b80d
Deepak Sarda <deepak@antrix.net>
2013-12-05 07:14:45 +0800

    fix static resources path

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@ export OUTPUT_DIR=${PUBLIC_DIR}.$(/bin/date +%F.%H-%M-%S)
 mkdir ${OUTPUT_DIR}
 python compile.py
 
-cp -r ${STATIC_DIR} ${OUTPUT_DIR}
+cp -r ${STATIC_DIR}/. ${OUTPUT_DIR}
 
 find ${OUTPUT_DIR} -type f | xargs chmod 644
 find ${OUTPUT_DIR} -type d | xargs chmod 755
